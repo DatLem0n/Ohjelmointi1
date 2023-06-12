21255b67cee5220d1f0e31915d656d7534fea6f5
DatLem0n <sahki232@gmail.com>
2023-06-12 20:33:20 +0300

    viikko5 alku

--- minitehtavat/main.c
+++ minitehtavat/main.c
@@ -1,7 +1,9 @@
 int mainViikko3(void);
 int mainViikko4(void);
+int mainViikko5(void);
 
 int main(){
     //mainViikko3();
     mainViikko4();
+    mainViikko5();
 }
---
+++ minitehtavat/minitehtavatViikko5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void tehtava22(void);
+
+int mainViikko5(){
+    tehtava22();
+}
+
+void tehtava22(){
+    int binaari;
+    printf("Anna viisinumeroinen binääriluku: ");
+    scanf("%i", &binaari)
+}
