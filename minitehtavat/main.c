4473c1355e7f01839f302b539f49d9fac847c0ae
Ville Kujala <sahki232@gmail.com>
2023-06-28 16:31:14 +0300

    viikko5

--- minitehtavat/main.c
+++ minitehtavat/main.c
@@ -1,9 +1,11 @@
 int mainViikko3(void);
 int mainViikko4(void);
 int mainViikko5(void);
+int mainViikko6(void);
 
 int main(){
     //mainViikko3();
     //mainViikko4();
-    mainViikko5();
+    //mainViikko5();
+    mainViikko6();
 }
